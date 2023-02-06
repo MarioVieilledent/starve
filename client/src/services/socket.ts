45fc0765a20f45809701ecec58d433e7a7d4cd87
Mario Vieilledent <mario.vieilledent@gmail.com>
2023-02-06 15:41:49 +0100

    location hostname for onrender host

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -3,4 +3,4 @@ import { io } from "socket.io-client";
 // const PORT: number = 5000;
 const PORT: number = 10000;
 
-export const socket = io(`ws://localhost:${PORT}`);
+export const socket = io(`ws://${window.location.hostname}:${PORT}`);
